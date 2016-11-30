053ca3f960dc646d1714ba9b491d33ec8bff7fdf
lrobinsonco <lrobinsonco@gmail.com>
2016-11-30 16:44:56 -0700

    google maps test
    cannot get api to load

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+     /* Additional Map Code Here */
+     center: {
+      lat: -13.1631412,
+      lng: -72.5471516
+    },
+    zoom: 14
+  });
+  }
+ })
