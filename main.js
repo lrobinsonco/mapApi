6213f5ee029cd44b9665db1e4e65074766f7607e
lrobinsonco <lrobinsonco@gmail.com>
2016-12-04 16:17:57 -0700

    changed to streetview

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
-$(document).ready(function () {
 
+
+// function initMap() {
+//   var home = {lat: 39.671342, lng: -105.036232}
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//      /* Additional Map Code Here */
+//      center: home
+//      //zoom: 19
+//   });
+//   var marker = new google.maps.Marker({
+//     position : home
+//     //map : map
+//   })
+//   }
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-     /* Additional Map Code Here */
-     center: {
-      lat: -13.1631412,
-      lng: -72.5471516
-    },
-    zoom: 14
-  });
-  }
- })
+       var home = {lat: 39.671322, lng: -105.036520};
+       var map = new google.maps.Map(document.getElementById('map'), {
+         zoom: 19,
+         center: home
+       });
+      //  var marker = new google.maps.Marker({
+      //    position: home,
+      //    map: map
+      //  });
+       var panorama = new google.maps.StreetViewPanorama(
+      document.getElementById('map'), {
+        position: home,
+        pov: {
+          heading: 85,
+          pitch: 9
+        }
+      });
+  map.setStreetView(panorama)
+     }
